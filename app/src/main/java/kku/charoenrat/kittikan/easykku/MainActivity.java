dad75ab7a77da5a3f799939f3538658cdd2533ca
Kittikan <ckittikan@kkumail.com>
2016-11-12 12:04:45 +0700

    Intent to SignUp finish

--- app/src/main/java/kku/charoenrat/kittikan/easykku/MainActivity.java
+++ app/src/main/java/kku/charoenrat/kittikan/easykku/MainActivity.java
@@ -1,13 +1,34 @@
 package kku.charoenrat.kittikan.easykku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private Button signInButton, sienUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        signInButton = (Button) findViewById(R.id.button2);
+        sienUpButton = (Button) findViewById(R.id.button);
+
+        //Sign Up Controller
+        sienUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SingUpActivity.class));
+            }
+        });
+
+
+    } //Main Method
+} //Main Class
